642b76e6cbd69faccafe3c956519fbac6bd06d62
achechet <achechet@gmail.com>
2022-02-14 11:01:57 +0300

    step - 1

---
+++ blockchain.py
@@ -0,0 +1,15 @@
+from block import Block
+import datetime
+
+block_chain = [Block.create_genesis_block()]
+print(("Genesis block has been created"))
+print("Hash: %s" % block_chain[-1].hash)
+
+num_blocks_to_add = 10
+
+for i in range(1, num_blocks_to_add+1):
+    block_chain.append(Block(block_chain[-1].hash, "DATA!", datetime.datetime.now()))
+    print("Block #%d has been created." % i)
+    print("Block #%d hash: %s" % (i, block_chain[i].hash))
+    
+    
